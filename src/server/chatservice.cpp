dedaa0a5a304df8644ffefb5f47847feaad63f84
Ni-Guvara <niguvara@outlook.com>
2025-08-22 10:13:43 +0800

    feat:1、网络模块与业务模块解耦；2、完成网络模块事件分发功能；3、完成业务模块事件回调功能；4、完成基本功能测试

--- src/server/chatservice.cpp
+++ src/server/chatservice.cpp
@@ -1,8 +1,15 @@
 #include "chatservice.hpp"
 
 #include "public.hpp"
+#include <muduo/base/Logging.h>
 
-ChatService::ChatService() {}
+using namespace placeholders;
+// 注册消息以及对应回调操作
+ChatService::ChatService()
+{
+    _msgHandlerMap.insert({LOGIN_MSG, std::bind(&ChatService::login, this, _1, _2, _3)});
+    _msgHandlerMap.insert({REG_MSG, std::bind(&ChatService::reg, this, _1, _2, _3)});
+}
 ChatService::~ChatService() {}
 
 ChatService *ChatService::instance()
@@ -11,12 +18,28 @@ ChatService *ChatService::instance()
     return &service;
 }
 
+MsgHandler ChatService::getHandler(int msgId)
+{
+    auto it = _msgHandlerMap.find(msgId);
+    if (it == _msgHandlerMap.end())
+    {
+        LOG_ERROR << "msgid:" << msgId << " can not find handler!";
+        return [=](const TcpConnectionPtr &conn, json &js, Timestamp time) {}; // 返回空操作, lambda
+    }
+    else
+    {
+        return _msgHandlerMap[msgId];
+    }
+}
+
 // 处理登陆业务
 void ChatService::login(const TcpConnectionPtr &conn, json &js, Timestamp time)
 {
+    LOG_INFO << "do login service!";
 }
 
 // 处理注册业务
 void ChatService::reg(const TcpConnectionPtr &conn, json &js, Timestamp time)
 {
+    LOG_INFO << "do reg service!";
 }
